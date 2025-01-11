566d3cf837fc96db41084be3618897775082f895
S <77365959+Numbone@users.noreply.github.com>
2025-01-11 22:36:37 +0500

    Time: 0 ms (100.00%) | Memory: 51.5 MB (63.20%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.ts
@@ -0,0 +1,11 @@
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+       for (let i = 0; i < n; i++) {
+        nums1[m + i] = nums2[i];
+    }
+
+    // Sort nums1 in-place
+    nums1.sort((a, b) => a - b);
+};
