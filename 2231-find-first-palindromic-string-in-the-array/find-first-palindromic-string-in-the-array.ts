a992e07c16c39e26b1ac5e1364635c0631ea2a65
Nurlybek Salkhayev <77365959+Numbone@users.noreply.github.com>
2025-04-24 23:45:53 +0500

    Time: 26 ms (14.63%) | Memory: 64.1 MB (7.32%) - LeetSync

---
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.ts
@@ -0,0 +1,8 @@
+function firstPalindrome(words: string[]): string {
+    for (const word of words) {
+    if (word === word.split('').reverse().join('')) {
+      return word;
+    }
+  }
+  return "";
+};
